c7f850ec7baa798aaf134c7a9bfd51760ed7dd94
Никита <killerlord1988@gmail.com>
2020-04-28 00:43:24 +0400

    Сделали сетку на все версии, добавили мелкие элементы, верстка приближенная к макету

---
+++ source/js/main-nav.js
@@ -0,0 +1,17 @@
+var headerMenu = document.querySelector(".page-header")
+var btn = document.querySelector(".main-nav");
+var menu = document.querySelector(".main-nav__items");
+var close = document.querySelector(".main-nav__toggle");
+
+headerMenu.classList.remove("no-js");
+console.log(btn);
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  menu.classList.toggle("menu-open");
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  close.classList.toggle("main-nav__toggle--close");
+});
